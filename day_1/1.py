b047c352408f167a270b85f8008d3d12cbf88fd9
andrew gorman <andrew.gorman@hydrogrid.eu>
2024-07-09 16:25:42 +0200

    initial commit

---
+++ day_1/1.py
@@ -0,0 +1,42 @@
+from typing import Tuple, List
+from functools import reduce
+import time
+import pathlib
+import re
+
+_TARGET_SUM = 2020
+
+
+def product_of_target_pair(file_path: str):
+    pair = _get_target_pair(file=file_path)
+    multiply = lambda x, y: x * y
+    return reduce(multiply, pair)
+
+
+def _get_target_pair(file: str) -> Tuple[int, int]:
+    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
+        lines = puzzle_input.read()
+        all_entries = list(map(int, re.findall(r"\d+", lines)))
+        return _get_pair_with_target_sum(all_entries=all_entries)
+
+
+def _get_pair_with_target_sum(
+    all_entries: List[int], target_sum: int = _TARGET_SUM
+) -> Tuple[int, int]:
+    visited = list()
+    for entry in all_entries:
+        delta = _TARGET_SUM - entry
+        if delta in visited:
+            return (entry, delta)
+        else:
+            visited.append(entry)
+    return (0, 0)
+
+
+start = time.perf_counter()
+print(product_of_target_pair("eg.txt"))
+print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+
+start = time.perf_counter()
+print(product_of_target_pair("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
