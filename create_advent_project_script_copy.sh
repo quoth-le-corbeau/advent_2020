cf0fcfb16cbc0b46accdc25997ac0c4359ced268
= <=>
2024-07-25 09:36:02 +0300

    add project create script copy for git

---
+++ create_advent_project_script_copy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "A New Year =============================================================================================================================" 
+echo "   _____                 __  .__                      _____       .___                    __            _____  _________            .___      
+  /  _  \   ____   _____/  |_|  |__   ___________    /  _  \    __| _/__  __ ____   _____/  |_    _____/ ____\ \_   ___ \  ____   __| _/____  
+ /  /_\  \ /    \ /  _ \   __\  |  \_/ __ \_  __ \  /  /_\  \  / __ |\  \/ // __ \ /    \   __\  /  _ \   __\  /    \  \/ /  _ \ / __ |/ __ \ 
+/    |    \   |  (  <_> )  | |   Y  \  ___/|  | \/ /    |    \/ /_/ | \   /\  ___/|   |  \  |   (  <_> )  |    \     \___(  <_> ) /_/ \  ___/ 
+\____|__  /___|  /\____/|__| |___|  /\___  >__|    \____|__  /\____ |  \_/  \___  >___|  /__|    \____/|__|     \______  /\____/\____ |\___  >
+        \/     \/                 \/     \/                \/      \/           \/     \/                              \/            \/    \/ "
+echo "Let's get this party started ==========================================================================================================="
+read -p "Enter the absolute path to your project: " project_path
+read -p "What year is it?: " project_year
+
+project_dir="${project_path}/advent_${project_year}"
+mkdir -p $project_dir || { echo "Creating ${project_dir} failed. Sorry!" ; exit 1; }
+
+solution_template=~/.scripts/aoc_solution_template.py || { echo "Finding ${solution_template} failed. Sorry!" ; exit 1; }
+
+for day_number in $(seq 25 $END); do 
+    project_day_dir="${project_dir}/day_${day_number}"
+    mkdir -p $project_day_dir || { echo "Creating ${project_day_dir} failed. Sorry!" ; exit 1; };
+    cat $solution_template > $project_day_dir/p1.py || { echo "Creating ${project_day_dir}/p1.py failed. Sorry!" ; exit 1; };
+    cat $solution_template > $project_day_dir/p2.py || { echo "Creating ${project_day_dir}/p2.py failed. Sorry!" ; exit 1; };
+    touch $project_day_dir/eg.txt || { echo "Creating ${project_day_dir}/eg.txt failed. Sorry!" ; exit 1; };
+    touch $project_day_dir/input.txt || { echo "Creating ${project_day_dir}/input.txt failed. Sorry!" ; exit 1; };
+done
