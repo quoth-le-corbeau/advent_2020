eb3213cd6fcf0c6e07c46d0411c435c80c87886d
= <=>
2024-07-26 11:34:01 +0300

    Begin day4

--- day_4/p1.py
+++ day_4/p1.py
@@ -1,22 +1,61 @@
+from typing import Optional, List
 import time
 import pathlib
+import dataclasses
+
+_FIELDS = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+    "cid",
+]
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+@dataclasses.dataclass(frozen=True)
+class Passport:
+    byr: str
+    iyr: str
+    eyr: str
+    hgt: str
+    hcl: str
+    ecl: str
+    pid: str
+    cid: Optional[str]
 
 
-def _RENAME_FUNC(file: str):
+def count_valid_passports(file_path: str) -> int:
+    passports = _get_passports(file=file_path)
+    return len(passports)
+
+
+def _get_passports(file: str) -> List[Passport]:
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        lines = puzzle_input.read().split("\n\n")
+        passports: List[Passport] = list()
+        for line in lines:
+            try:
+                passports.append(_make_passport(fields_string=line))
+            except Exception:
+                continue
+        return passports
+
+
+def _make_passport(fields_string: str) -> Passport:
+    print("new passport:")
+    fields = fields_string.split(" ")
+    for field in fields:
+        print(field)
+    print("===========")
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(count_valid_passports("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
 # start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
+# print(count_valid_passports("input.txt"))
 # print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
