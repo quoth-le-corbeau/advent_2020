3d5c90e05fd27742cc6217867e69ac81feeb3df8
andrew gorman <andrew.gorman@hydrogrid.eu>
2024-07-27 16:06:17 +0200

    Complete day_5 p1

--- day_5/p1.py
+++ day_5/p1.py
@@ -3,28 +3,63 @@ import time
 import pathlib
 
 
+
 def get_highest_seat_id(file_path: str) -> int:
     boarding_passes = _get_boarding_passes(file=file_path)
     print(f"{boarding_passes=}")
     seat_ids: List[int] = list()
     for boarding_pass in boarding_passes:
-        row_number = _get_row_number(binary_code=boarding_pass[0])
-        column_number = _get_column_number(binary_code=boarding_pass[1])
-        seat_ids.append(row_number * 8 * column_number)
+        row_number = _do_specificied_binary_search(
+            binary_code=boarding_pass[0],
+            starting_range_size=128,
+            starting_binary_index=6,
+            take_lower_signifier="F",
+            take_upper_signifier="B",
+        )
+        print(f"{row_number=}")
+        column_number = _do_specificied_binary_search(
+            binary_code=boarding_pass[1],
+            starting_range_size=8,
+            starting_binary_index=2,
+            take_lower_signifier="L",
+            take_upper_signifier="R",
+        )
+        print(f"{column_number=}")
+        seat_ids.append(row_number * 8 + column_number)
     return max(seat_ids)
 
 
-def _get_row_number(binary_code: str) -> int:
-    """
-    binary search algo with 0 - 127
-    0101100
-    
-    """
-    return 1
+# TODO: research use of overload in case relevant
+def _do_specificied_binary_search(
+    binary_code: str,
+    starting_range_size: int,
+    starting_binary_index: int,
+    take_upper_signifier: str,
+    take_lower_signifier: str,
+) -> int:
+    assert len(binary_code) in [3, 7]
+    lower_bound = 0
+    upper_bound = lower_bound + starting_range_size - 1
+    i = 0
+    char = "x"
+    while len(range(lower_bound, upper_bound)) >= 2:
+        range_size = 2 ** (starting_binary_index - i)
+        char = binary_code[i]
+        if char == take_upper_signifier:
+            lower_bound = lower_bound + range_size
+        elif char == take_lower_signifier:
+            upper_bound = lower_bound + range_size - 1
+        i += 1
 
-
-def _get_column_number(binary_code: str) -> int:
-    return 1
+    char = binary_code[i]
+    if char == take_upper_signifier:
+        print(f"taking upper bound: {upper_bound}")
+        return upper_bound
+    elif char == take_lower_signifier:
+        print(f"taking lower bound: {lower_bound}")
+        return lower_bound
+    else:
+        raise RuntimeError("Incorrect take_upper or take_lower identifier!")
 
 
 def _get_boarding_passes(file: str) -> List[Tuple[str, str]]:
@@ -40,6 +75,6 @@ start = time.perf_counter()
 print(get_highest_seat_id("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(get_highest_seat_id("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(get_highest_seat_id("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
