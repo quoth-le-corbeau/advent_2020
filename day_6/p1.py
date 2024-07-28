1a42739335f1fab999f7823805ba08a63dc5fab0
quoth-the-macbook <amgorman@tutanota.com>
2024-07-28 21:29:29 +0200

    Complete day 6

--- day_6/p1.py
+++ day_6/p1.py
@@ -1,22 +1,29 @@
+from typing import List, Set
 import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def sum_of_yes_counts(file_path: str) -> int:
+    grouped_yesses = _count_yesses(file=file_path)
+    count = 0
+    for group in grouped_yesses:
+        count += len(group)
+    return count
 
 
-def _RENAME_FUNC(file: str):
+def _count_yesses(file: str) -> List[Set[str]]:
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        people = puzzle_input.read().split("\n\n")
+        yesses = list()
+        for person in people:
+            yesses.append(set("".join(person.split())))
+        return yesses
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(sum_of_yes_counts("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(sum_of_yes_counts("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
