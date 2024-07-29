7bdb484723637eba47bee02ff4ea0975b042b58c
andrew gorman <andrew.gorman@hydrogrid.eu>
2024-07-29 18:09:54 +0200

    begin day7

--- day_7/p1.py
+++ day_7/p1.py
@@ -1,22 +1,39 @@
 import time
 import pathlib
 
-
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+_SHINY_GOLD = "shiny gold bag"
 
 
-def _RENAME_FUNC(file: str):
+def get_number_of_shiny_gold_bag_holders(file_path: str):
+    rules = _get_bag_holding_rules(file=file_path)
+    print(f"{rules=}")
+    count = 0
+    bags_containing_gold = list()
+    for color, contains in rules.items():
+        if any(_SHINY_GOLD in contents for contents in contains):
+            bags_containing_gold.append(color)
+            count += 1
+    print(f"{bags_containing_gold=}")
+    for bag in bags_containing_gold:
+        count += list(rules.keys()).count(bag)
+    return count
+
+
+def _get_bag_holding_rules(file: str):
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        lines = puzzle_input.read().splitlines()
+        rules_by_bag = dict()
+        for line in lines:
+            line = line.replace(" contain", ",").replace(".", "").replace("bags", "bag")
+            split_line = line.split(",")
+            rules_by_bag[split_line[0]] = split_line[1:]
+        return rules_by_bag
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(get_number_of_shiny_gold_bag_holders("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(get_number_of_shiny_gold_bag_holders("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
